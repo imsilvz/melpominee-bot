5e0bc9404c94a06744bbacd0be8bf3e637d2eff1
Richard Yawger <rjyawger@me.com>
2023-03-05 18:28:54 -0500

    workaround

--- src/voice.js
+++ src/voice.js
@@ -37,6 +37,19 @@ export const JoinDiscordVoiceChannel = async (channel) => {
             await UpdateMusicEmbed(channel.guild);
         }
     });
+
+    conn.on('stateChange', (oldState, newState) => {
+        const oldNetworking = Reflect.get(oldState, 'networking');
+        const newNetworking = Reflect.get(newState, 'networking');
+      
+        const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+          const newUdp = Reflect.get(newNetworkState, 'udp');
+          clearInterval(newUdp?.keepAliveInterval);
+        }
+      
+        oldNetworking?.off('stateChange', networkStateChangeHandler);
+        newNetworking?.on('stateChange', networkStateChangeHandler);
+    });
 }
 
 async function helper_GetMusicMetadata(filepath) {
