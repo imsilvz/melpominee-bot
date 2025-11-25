c798434da309de418481bfb95bb72fcc8d4f7443
Richard Yawger <rjyawger@me.com>
2025-11-24 20:47:42 -0500

    add test command

---
+++ src/Commands/TestCommandModule.cs
@@ -0,0 +1,23 @@
+﻿using NetCord;
+using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+using Melpominee.Utility;
+
+namespace Melpominee.Commands;
+public class TestCommandModule : ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SlashCommand("ping", "Pong", Register = true)]
+    public async Task Ping()
+    {
+        await RespondAsync(
+            InteractionCallback.Message(
+                new()
+                {
+                    Content = "Pong!",
+                    Flags = MessageFlags.Ephemeral
+                }
+            )
+        );
+        return;
+    }
+}
